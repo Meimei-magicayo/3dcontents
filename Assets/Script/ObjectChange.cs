69f24a6d43d4aae18caca454b2e5a3e43515fa95
Meimei-magicayo <137146762+Meimei-magicayo@users.noreply.github.com>
2023-10-16 10:53:34 +0900

    オブジェクトランダムに出現成功

--- Assets/Script/ObjectChange.cs
+++ Assets/Script/ObjectChange.cs
@@ -6,12 +6,17 @@ public class ObjectChange : MonoBehaviour
 {
     [SerializeField]
     public GameObject[] gameObjects;
+    public int i,n,a,b,c,d;
+    public int Randnumber;
     public int number=0;
-    public int i;
+    int start = 0;
+    int end = 3;
+
+    List<int> numbers = new List<int>();
     // Start is called before the first frame update
     void Start()
-    { 
-
+    {
+        n = 1;
     }
 
     // Update is called once per frame
@@ -22,7 +27,66 @@ public class ObjectChange : MonoBehaviour
     GameObject OldObj;
     public void OnClick()
     {
-        if (number == 0)
+        for (int i = start; i <= end; i++)
+        {
+            numbers.Add(i);
+        }
+
+        while (numbers.Count > 0)
+        {
+
+            int index = Random.Range(0, numbers.Count);
+
+            int ransu = numbers[index];
+            if (ransu == 0)
+            {
+                Destroy(OldObj);
+                OldObj = Instantiate(gameObjects[ransu], new Vector3(0, 0, 0), Quaternion.identity);
+                //a = 0;
+                break;
+            }
+
+
+
+            else if (ransu == 1)
+            {
+                if (number != 0)
+                {
+                    Destroy(OldObj);
+                }
+
+                OldObj = Instantiate(gameObjects[ransu], new Vector3(0, 0, 0), Quaternion.identity);
+                //b = 0;
+                break;
+            }
+            else if (ransu == 2)
+            {
+                if (number != 0)
+                {
+                    Destroy(OldObj);
+                }
+                OldObj = Instantiate(gameObjects[ransu], new Vector3(0, 0, 0), Quaternion.identity);
+                break;
+            }
+            else if (ransu== 3)
+            {
+                if (number != 0)
+                {
+                    Destroy(OldObj);
+                }
+                OldObj = Instantiate(gameObjects[ransu], new Vector3(0, 0, 0), Quaternion.identity);
+            }
+            numbers.RemoveAt(index);
+            break;
+        }
+
+
+        
+
+         number++;
+
+
+        /*if (number == 0)
         {
             OldObj=Instantiate(gameObjects[0], new Vector3(0, 0, 0), Quaternion.identity);
             
@@ -48,7 +112,13 @@ public class ObjectChange : MonoBehaviour
             OldObj = Instantiate(gameObjects[number], new Vector3(0, 0, 0), Quaternion.identity);
         }
 
-      number++;
+      number++;*/
+    }
+
+    public void OnReset()
+    {
+
+        OldObj.transform.Rotate(0, 0, 0);
     }
 }
 
