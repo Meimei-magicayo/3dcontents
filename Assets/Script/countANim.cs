c518102194bc22e0b541ad3e6783948bc1cd4b7f
Meimei-magicayo <137146762+Meimei-magicayo@users.noreply.github.com>
2023-10-18 16:07:30 +0900

    10/18終了

--- Assets/Script/countANim.cs
+++ Assets/Script/countANim.cs
@@ -2,15 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class countANim : MonoBehaviour
+public class countanim : MonoBehaviour
 {
     float time;
     Animator animator;
-    int i=0;
+    int i = 0;
     // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponent<Animator>();   
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,20 +19,24 @@ public class countANim : MonoBehaviour
         time += Time.deltaTime;
         if (time % 3 == 0)
         {
-           
+            i++;
             if (i == 1)
             {
-                animator.SetBool("Orc", true);
-           }
-            else if(i==3)
+                animator.SetBool("orc", true);
+            }
+
+            else if (i == 3)
             {
-                animator.SetBool("Taking", true);
+                animator.SetBool("Talk", true);
+            }
+            else if (i == 5)
+            {
+                i = 0;
             }
             else
             {
                 animator.SetBool("Idle", true);
             }
-            i=i+1;
         }
     }
 }
