13cce0d6c4bb8f80c2cd5b6cc9e2331640c86868
Meimei-magicayo <137146762+Meimei-magicayo@users.noreply.github.com>
2023-11-14 20:28:27 +0900

    Revert "y"
    This reverts commit e9f6e049f50a9dcc06ab13e6a558fb206d9cffed.

--- Assets/Script/ObjectChange.cs
+++ Assets/Script/ObjectChange.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class ObjectChange : MonoBehaviour
 {
@@ -15,23 +14,16 @@ public class ObjectChange : MonoBehaviour
     public int i,n,a,b,c,d;
     public int Randnumber;
     public int number=0;
-    int start = 1;
-    int end = 15;
-    int count = 1;
-
- 
-
+    int start = 0;
+    int end = 3;
 
     List<int> numbers = new List<int>();
-
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = start; i <= end; i++)
-        {
-            numbers.Add(i);
-        }
-        n = 1;   
+
+
+        n = 1;
     }
 
     // Update is called once per frame
@@ -42,112 +34,173 @@ public class ObjectChange : MonoBehaviour
     GameObject OldObj;
     public void OnClick()
     {
+        /* for (int i = start; i <= end; i++)
+         {
+             numbers.Add(i);
+         }
+
+
+         while (n==1) { 
+             int index = Random.Range(0, numbers.Count);
+
+             int ransu = numbers[index];
+
+             if (ransu == 0 && a != 0)
+             {
+                 if (number != 0)
+                 {
+                     Destroy(OldObj);
+                 }
+                 OldObj = Instantiate(gameObjects[ransu], new Vector3(0, 0, 0), Quaternion.identity);
+                 numbers.RemoveAt(index);
+                 n = 0;
+
+             }
+
+
+
+             else if (ransu == 1 )
+             {
+                 if (number != 0)
+                 {
+                     Destroy(OldObj);
+                 }
+
+                 OldObj = Instantiate(gameObjects[ransu], new Vector3(0, 0, 0), Quaternion.identity);
+                 numbers.RemoveAt(index);
+                 n = 0;
+
+             }
+             else if (ransu == 2)
+             {
+                 if (number != 0)
+                 {
+                     Destroy(OldObj);
+                 }
+                 OldObj = Instantiate(gameObjects[ransu], new Vector3(0, 0, 0), Quaternion.identity);
+                 numbers.RemoveAt(index);
+                 n = 0;
+
+             }
+             else if (ransu== 3)
+             {
+                 if (number != 0)
+                 {
+                     Destroy(OldObj);
+                 }
+                 OldObj = Instantiate(gameObjects[ransu], new Vector3(0, 0, 0), Quaternion.identity);
+                 numbers.RemoveAt(index);
+                 n = 0;
+             }        
 
 
 
 
 
+         }
 
-        if (number >=15)
+
+
+
+          number++;*/
+
+        if (number >=gameObjects.Length)
         {
             SceneManager.LoadScene("tytle");
         }
-        int index = Random.Range(0, numbers.Count);
-
-            int ransu = numbers[index];
-        Debug.Log(ransu);
-
-        if (number== 0)
+        if (number == 0)
         {
-  
-            OldObj = Instantiate(gameObjects[ransu], new Vector3(0, 0,-460), Quaternion.Euler(0, 0, 0));
+            OldObj = Instantiate(gameObjects[number], new Vector3(0, 0,-460), Quaternion.Euler(0, 0, 0));
             //OldObj=Instantiate(gameObjects[0], new Vector3(0, -10, -460), Quaternion.Euler(0,180,0));
- 
+
         }
            
             
 
-        else if (number== 1)
+        else if (number == 1)
+        {
             Destroy(OldObj);
-            OldObj = Instantiate(gameObjects[ransu], new Vector3(0, 0, -460), Quaternion.Euler(0, 0, 0));
-      
+            OldObj = Instantiate(gameObjects[number], new Vector3(0, 0, -460), Quaternion.Euler(0, 0, 0));
         }
         else if (number == 2)
         {
             Destroy(OldObj);
-            OldObj = Instantiate(gameObjects[ransu], new Vector3(0, 0, -460), Quaternion.Euler(0, 0, 0));
+            OldObj = Instantiate(gameObjects[number], new Vector3(0, 0, -460), Quaternion.Euler(0, 0, 0));
         }
         else if (number == 3)
         {
             Destroy(OldObj);
-            OldObj = Instantiate(gameObjects[ransu], new Vector3(0, 0, -460), Quaternion.Euler(0, 0, 0));
+            OldObj = Instantiate(gameObjects[number], new Vector3(0, 0, -460), Quaternion.Euler(0, 0, 0));
         }
         else if (number == 4)
         {
             Destroy(OldObj);// kodomo
-            OldObj = Instantiate(gameObjects[ransu], new Vector3(0, 0, -460), Quaternion.Euler(0, 0, 0));
+            OldObj = Instantiate(gameObjects[number], new Vector3(0, 0, -460), Quaternion.Euler(0, 0, 0));
         }
         else if (number == 5)
         {
             Destroy(OldObj);//kodomo
-            OldObj = Instantiate(gameObjects[ransu], new Vector3(0, 0, -460), Quaternion.Euler(0, 0, 0));
+            OldObj = Instantiate(gameObjects[number], new Vector3(0, 0, -460), Quaternion.Euler(0, 0, 0));
         }
         else if (number == 6)
         {
             Destroy(OldObj);
-            OldObj = Instantiate(gameObjects[ransu], new Vector3(0, 0, -460), Quaternion.Euler(0, 0, 0));
+            OldObj = Instantiate(gameObjects[number], new Vector3(0, 0, -460), Quaternion.Euler(0, 0, 0));
         }
         else if (number == 7)
         {
             Destroy(OldObj);
-            OldObj = Instantiate(gameObjects[ransu], new Vector3(0, 0, -460), Quaternion.Euler(0, 0, 0));
+            OldObj = Instantiate(gameObjects[number], new Vector3(0, 0, -460), Quaternion.Euler(0, 0, 0));
         }
         else if (number == 8)
         {
             Destroy(OldObj);
-            OldObj = Instantiate(gameObjects[ransu], new Vector3(0, 0, -460), Quaternion.Euler(0, 0, 0));
+            OldObj = Instantiate(gameObjects[number], new Vector3(0, 0, -460), Quaternion.Euler(0, 0, 0));
         }
         else if (number == 9)
         {
             Destroy(OldObj);
-            OldObj = Instantiate(gameObjects[ransu], new Vector3(0, 0, -460), Quaternion.Euler(0, 0, 0));
+            OldObj = Instantiate(gameObjects[number], new Vector3(0, 0, -460), Quaternion.Euler(0, 0, 0));
         }
         else if (number == 10)
         {
             Destroy(OldObj);// kodomo
-            OldObj = Instantiate(gameObjects[ransu], new Vector3(0, 0, -460), Quaternion.Euler(0, 0, 0));
+            OldObj = Instantiate(gameObjects[number], new Vector3(0, 0, -460), Quaternion.Euler(0, 0, 0));
         }
         else if (number == 11)
         {
             Destroy(OldObj);//kodomo
-            OldObj = Instantiate(gameObjects[ransu], new Vector3(0, 0, -460), Quaternion.Euler(0, 0, 0));
+            OldObj = Instantiate(gameObjects[number], new Vector3(0, 0, -460), Quaternion.Euler(0, 0, 0));
         }
         else if (number == 12)
         {
             Destroy(OldObj);
-            OldObj = Instantiate(gameObjects[ransu], new Vector3(0, 0, -460), Quaternion.Euler(0, 0, 0));
+            OldObj = Instantiate(gameObjects[number], new Vector3(0, 0, -460), Quaternion.Euler(0, 0, 0));
         }
         else if (number == 13)
         {
             Destroy(OldObj);
-            OldObj = Instantiate(gameObjects[ransu], new Vector3(0, 0, -460), Quaternion.Euler(0, 0, 0));
+            OldObj = Instantiate(gameObjects[number], new Vector3(0, 0, -460), Quaternion.Euler(0, 0, 0));
         }
         else if (number == 14)
         {
             Destroy(OldObj);
-            OldObj = Instantiate(gameObjects[ransu], new Vector3(0, 0, -460), Quaternion.Euler(0, 0, 0));
+            OldObj = Instantiate(gameObjects[number], new Vector3(0, 0, -460), Quaternion.Euler(0, 0, 0));
         }
         else if (number == 15)
         {
             Destroy(OldObj);
-            OldObj = Instantiate(gameObjects[ransu], new Vector3(0, 0, -460), Quaternion.Euler(0, 0, 0));
+            OldObj = Instantiate(gameObjects[number], new Vector3(0, 0, -460), Quaternion.Euler(0, 0, 0));
         }
-        
+
         number++;
-        numbers.RemoveAt(index);
     }
 
-
+    public void OnReset()
+    {
+        gameObjects[number].transform.rotation = initialRot[number];
+       gameObjects[number].transform.position = initialPos[number];
+    }
 }
 
 
